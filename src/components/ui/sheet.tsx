f46339f2d54757c04a6d3abc6984577151459284
vinmasci <vincentmasci@icloud.com>
2025-01-07 17:52:12 +1100

    <

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -51,7 +51,7 @@ interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {}
 
-const SheetContent = React.forwardRef
+const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
 >(({ side = "right", className, children, ...props }, ref) => (
